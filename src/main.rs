852fb4b939b11390c6061c8b3778d39679d6311c
firasastwani <firasastwani1@gmail.com>
2026-03-02 16:18:40 -0500

    mmap impl, run time: 5.1 sec

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use core::f64;
 use memmap2::Mmap;
 use std::collections::HashMap;
 use std::fs::File;
-use std::io::{BufRead, BufReader};
 
 struct Info {
     min: f64,
@@ -29,10 +28,14 @@ fn main() {
     // read the data in the file line by line
     let file = File::open("../data/measurements.txt").unwrap();
     // in the future, allocate a buffer with_capacity = to the bytes in the file
-    let reader = BufReader::new(file);
 
-    for line in reader.split(b'\n') {
-        let line = line.unwrap();
+    let mmap = unsafe { Mmap::map(&file) }.unwrap();
+
+    for line in mmap.split(|c| *c == b'\n') {
+        if line.is_empty() {
+            break;
+        }
+
         let (station, temp) = split_lines(&line);
 
         // only allcate on new entries
