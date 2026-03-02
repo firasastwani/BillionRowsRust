60e818eb16c8734a5a6d286137b427f1f0c92b92
firasastwani <firasastwani1@gmail.com>
2026-03-02 15:49:22 -0500

    move from String keys to Vec<u8> keys. Slower now but using mmap will make it worthwhile

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use core::f64;
-use memmap2::Mmap;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
@@ -24,22 +23,22 @@ impl Default for Info {
 
 fn main() {
     // TODO: Store the key as a Vec<u8> instead for speed.
-    let mut map: HashMap<String, Info> = HashMap::with_capacity(10_000);
+    let mut map: HashMap<Vec<u8>, Info> = HashMap::with_capacity(10_000);
 
     // read the data in the file line by line
     let file = File::open("../data/measurements.txt").unwrap();
     // in the future, allocate a buffer with_capacity = to the bytes in the file
     let reader = BufReader::new(file);
 
-    for line in reader.lines() {
+    for line in reader.split(b'\n') {
         let line = line.unwrap();
         let (station, temp) = split_lines(&line);
 
         // only allcate on new entries
 
-        let stats = match map.get_mut(station) {
+        let stats = match map.get_mut(&station) {
             Some(stats) => stats,
-            None => map.entry(station.to_string()).or_default(),
+            None => map.entry(station.to_vec()).or_default(),
         };
 
         stats.min = stats.min.min(temp);
@@ -47,12 +46,13 @@ fn main() {
         stats.count += 1;
         stats.total += temp;
     }
-    let mut sorted: Vec<(String, Info)> = map.into_iter().collect();
+    let mut sorted: Vec<(Vec<u8>, Info)> = map.into_iter().collect();
     sorted.sort_by(|a, b| a.0.cmp(&b.0));
 
     print!("{{");
     for (station, info) in sorted {
         let mean = info.total / info.count as f64;
+        let station = std::str::from_utf8(&station).unwrap();
 
         print!("{station} = {}/{:.1}/{}", info.min, mean, info.max);
 
@@ -62,11 +62,12 @@ fn main() {
     print!("}}");
 }
 
-// takes a line from the file and returns a tuple of String, Temperature
-fn split_lines(line: &str) -> (&str, f64) {
-    let (station, temp) = line.split_once(';').unwrap();
+fn split_lines(line: &[u8]) -> (Vec<u8>, f64) {
+    // splitn(2) on ';' gives [station, temp] in forward order
+    let mut fields = line.splitn(2, |c| *c == b';');
+    let station = fields.next().unwrap();
+    let temp = fields.next().unwrap();
+    let temp: f64 = std::str::from_utf8(temp).unwrap().parse().unwrap();
 
-    let temp: f64 = temp.parse().unwrap();
-
-    return (station, temp);
+    (station.to_vec(), temp)
 }
