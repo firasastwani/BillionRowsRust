9ce16b3df673971c748489f963e44baca9a59e17
firasastwani <firasastwani1@gmail.com>
2026-03-01 22:54:32 -0500

    basic impl, rt 10.9sec

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
+use core::f64;
+use std::collections::{BTreeMap, HashMap};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader, Result};
+use std::path::Path;
+
+struct Info {
+    min: f64,
+    count: usize,
+    total: f64,
+    max: f64,
+}
+
+impl Default for Info {
+    fn default() -> Self {
+        Info {
+            min: f64::INFINITY,
+            max: f64::NEG_INFINITY,
+            count: 0,
+            total: 0.0,
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    // do we want it to take a str slice? can change later
+    let mut map = BTreeMap::<String, Info>::new();
+
+    // read the data in the file line by line
+    let file = File::open("../data/measurements.txt").unwrap();
+    // in the future, allocate a buffer with_capacity = to the bytes in the file
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        let (station, temperature) = split_lines(&line.unwrap());
+
+        let stats = map.entry(station).or_default();
+        stats.min = stats.min.min(temperature);
+        stats.count += 1;
+        stats.total += temperature;
+        stats.max = stats.max.max(temperature);
+    }
+
+    print!("{{");
+    let mut stats = map.into_iter().peekable();
+    while let Some((station, info)) = stats.next() {
+        let mean = info.total / info.count as f64;
+
+        print!("{station} = {}/{}/{}", info.min, mean, info.max);
+
+        if stats.peek().is_some() {
+            print!(", ")
+        }
+    }
+
+    print!("}}");
+}
+
+// takes a line from the file and returns a tuple of String, Temperature
+fn split_lines(line: &str) -> (String, f64) {
+    let mut parts = line.split(';');
+
+    let station = parts.next().unwrap().to_string();
+    let temp_str = parts.next().unwrap();
+    let temp: f64 = temp_str.trim().parse::<f64>().unwrap();
+
+    return (station, temp);
 }
